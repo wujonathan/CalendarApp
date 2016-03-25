fe9ac9511aec412950b6bd579e52308f4743dd19
Jonathan Wu <wu.jonathan@wustl.edu>
2016-03-25 17:11:46 -0500

    Added js

---
+++ createuser_ajax.js
@@ -0,0 +1,22 @@
+function loginAjax(event){
+	var username = document.getElementById("username").value; // Get the username from the form
+	var password = document.getElementById("password").value; // Get the password from the form
+ 
+	// Make a URL-encoded string for passing POST data:
+	var dataString = "username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password);
+ 
+	var xmlHttp = new XMLHttpRequest(); // Initialize our XMLHttpRequest instance
+	xmlHttp.open("POST", "createuser_ajax.php", true); // Starting a POST request (NEVER send passwords as GET variables!!!)
+	xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); // It's easy to forget this line for POST requests
+	xmlHttp.addEventListener("load", function(event){
+		var jsonData = JSON.parse(event.target.responseText); // parse the JSON into a JavaScript object
+		if(jsonData.success){  // in PHP, this was the "success" key in the associative array; in JavaScript, it's the .success property of jsonData
+			alert("You created a new user!");
+		}else{
+			alert("You failed to create a new user.  "+jsonData.message);
+		}
+	}, false); // Bind the callback to the load event
+	xmlHttp.send(dataString); // Send the data
+}
+ 
+document.getElementById("submitCreateUser").addEventListener("click", loginAjax, false); // Bind the AJAX call to button click
