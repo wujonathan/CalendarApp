03d500346798c545e28d6dd048f9baf78a04b86a
Jonathan Wu <wu.jonathan@wustl.edu>
2016-03-25 21:37:44 -0500

    fixed addevent

--- addevent_ajax.php
+++ addevent_ajax.php
@@ -5,6 +5,22 @@ session_start();
 $previous_ua = @$_SESSION['useragent'];
 $current_ua = $_SERVER['HTTP_USER_AGENT'];
 
+function existingUserCheck($u){
+	require 'database.php';
+	$stmt = $mysqli->prepare("SELECT COUNT(*) FROM registered_users WHERE username = (?) LIMIT 1");
+	if(!$stmt){
+		printf("Query Prep Failed: %s\n", $mysqli->error);
+		exit;
+	}
+	$stmt->bind_param('s', $u);
+	$stmt->execute();
+	$stmt->bind_result($result);
+	$stmt->fetch();
+	$stmt->close();
+	if($result==0){return false;}
+	else{return true;}
+}
+
 if(isset($_SESSION['useragent']) && $previous_ua !== $current_ua){
 	die("Session hijack detected");
 }else{
@@ -50,22 +66,30 @@ else{
 	$stmt->close();
 
 	for(int $i=0;$i<sizeof($groups);$i++){
-		$curU=$groups[$i];
-		$stmt = $mysqli->prepare("SELECT user_id FROM registered_users WHERE username=?");
-		$stmt->bind_param('s', $cur_id);
-		$stmt->execute();
-		$stmt->bind_result($user_id, $pwd_hash);
-		$stmt->fetch();
-		$stmt->close();
-		
-		$stmt = $mysqli->prepare("INSERT INTO group_events (event_id, user_id) VALUES (?, ?)");
-		if(!$stmt){
-			printf("Query Prep Failed: %s\n", $mysqli->error);
-			exit;
+		if(existingUserCheck($groups[$i])){
+			$curU=$groups[$i];
+			$stmt = $mysqli->prepare("SELECT user_id FROM registered_users WHERE username=?");
+			$stmt->bind_param('s', $cur_id);
+			$stmt->execute();
+			$stmt->bind_result($user_id, $pwd_hash);
+			$stmt->fetch();
+			$stmt->close();
+
+			$stmt = $mysqli->prepare("INSERT INTO group_events (event_id, user_id) VALUES (?, ?)");
+			if(!$stmt){
+				printf("Query Prep Failed: %s\n", $mysqli->error);
+				exit;
+			}
+			$stmt->bind_param('ii', $event_id, $cur_id);
+			$stmt->execute();
+			$stmt->close();
 		}
-		$stmt->bind_param('ii', $event_id, $cur_id);
-		$stmt->execute();
-		$stmt->close();
+		else{
+			echo json_encode(array(
+				"success" => false,
+				"message" => $groups[$i]
+				));
+		}
 	}
 }
 
