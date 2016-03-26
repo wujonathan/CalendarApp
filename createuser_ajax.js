da952eda87d2140c81bb63bb6f7c67e583297b26
Jonathan Wu <wu.jonathan@wustl.edu>
2016-03-26 13:59:40 -0500

    no message

--- createuser_ajax.js
+++ createuser_ajax.js
@@ -1,9 +1,9 @@
 function createuserAjax(event){
-	var username = document.getElementById("username").value; // Get the username from the form
-	var password = document.getElementById("password").value; // Get the password from the form
+	var username = document.getElementById("newUsername").value; // Get the username from the form
+	var password = document.getElementById("newPassword").value; // Get the password from the form
  
 	// Make a URL-encoded string for passing POST data:
-	var dataString = "username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password);
+	var dataString = "newUsername=" + encodeURIComponent(username) + "&newPassword=" + encodeURIComponent(password);
  
 	var xmlHttp = new XMLHttpRequest(); // Initialize our XMLHttpRequest instance
 	xmlHttp.open("POST", "createuser_ajax.php", true); // Starting a POST request (NEVER send passwords as GET variables!!!)
@@ -12,8 +12,8 @@ function createuserAjax(event){
 		var jsonData = JSON.parse(event.target.responseText); // parse the JSON into a JavaScript object
 		if(jsonData.success){  // in PHP, this was the "success" key in the associative array; in JavaScript, it's the .success property of jsonData
 			alert("You created a new user!");
-			document.getElementById("username").value="";
-			document.getElementById("password").value="";
+			document.getElementById("newUsername").value="";
+			document.getElementById("newPassword").value="";
 		}else{
 			alert("You failed to create a new user.  "+jsonData.message);
 		}
