b5d3affa2e9b8421e0051f9cf01d7938d74a849f
Manil Bastola <mbastola@ip-172-31-28-83.us-west-2.compute.internal>
2016-03-25 19:26:55 -0500

    new

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <meta charset="UTF-8" />
   <title>Calendar</title>
   <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" type="text/javascript"></script>
 </head>
 
 
@@ -20,8 +21,8 @@
   <div>
     <form id="userlogin" action="#">
       <br> Please Input the details for login: </br><br></br>
-      Username <input type="text"  name="username">  <br>
-      Password <input type="password"  name="password">  <br><br></br>
+      Username <input type="text"  name="username" id="username">  <br>
+      Password <input type="password"  name="password" id="password">  <br><br></br>
       <input id="submitLogin" type="submit" name="submit" value="Create User"/>
     </form>
   </div>
@@ -30,8 +31,8 @@
   <div>
     <form id="userCreate" action="#">
       <br> Please Input the details for registering: </br><br></br>
-      Username <input type="text"  name="newUsername">  <br>
-      Password <input type="password"  name="newPassword">  <br><br></br>
+      Username <input type="text"  name="newUsername" id="newUsername">  <br>
+      Password <input type="password"  name="newPassword" id="newPassword">  <br><br></br>
       <input id="submitCreateUser" type="submit" name="submit" value="Create User"/>
     </form>
   </div>
@@ -44,97 +45,62 @@
   </div>
   <div class="addEvent">
       <button class="btns" id="createEvent">Create Event</button>
+      <button class="btns" id="shareCalendar">Share My Cal</button>
   </div>
   <div class="eventCreateDetails">
     <div>
     <form id="newEvent" action="#">
       <br> Please Input the details of event: </br><br></br>
-      Username <input type="text"  name="username">  <br>
-      Password <input type="password"  name="password">  <br><br></br>
-      <input id="submitLogin" type="submit" name="submit" value="Create User"/>
+      Title <input type="text"  name="eventTitle">  <br>
+      Day <input type="date"  name="eventDate"> <br>
+      Time <input type="time"  name="eventTime"> <br>
+      <textarea rows="4" cols="50" name="comment" placeholder="Enter Description Here.."></textarea>  <br>
+      <button class="btns" id="inviteOthers">Invite others</button><br><br></br>
+      <input id="submitNewEvent" type="submit" name="submit" value="Create Event"/>
+    </form>
+    </div>
+  </div>
+  <div class="shareDetails">
+    <div>
+    <form id="newShare" action="#">
+      <br> Please input the details for sharing your calendar: </br><br></br>
+      Share With: <input type="text"  name="shareWith">  <br>
+      <button class="btns" id="addMore">Add more</button><br><br></br>
+      <input id="submitShare" type="submit" name="submit" value="Share"/>
     </form>
     </div>
   </div>
 </div>
 </div>
-  <?php  
-     require 'database.php';
- $stmt4 = $mysqli->prepare("SELECT news_id, story, local, path, user_id FROM news WHERE day(last_update)=day(NOW()) and month(last_update)=month(NOW()) and year(last_update)=year(NOW()) order by RAND() LIMIT 1");
-
-  if(!$stmt4){
-  printf("Query Prep Failed: %s\n", $mysqli->error);
-  exit;
-  }
-  $stmt4->execute();
-  $stmt4->bind_result($news_id, $text, $local, $path, $user_id);
-  $stmt4->fetch();
-  printf("<div class='layer2'><div class='label'>Random Story of the Day:</div>\n<ul><div class='holder'><div class='story'>%s</div>\n",$text);
-  if ($path != ""){
-   printf("<a href=%s> %s </a>\n",$path, $path);
-        }
-   printf("</div></ul>\n<div><form action='viewStory.php' method='post'><input type='hidden' name='news_id' value='%s'/><input type='submit' class='editComment' name='viewStory' value='Expand'/></form></div></div>\n<div class='layer1'>", $news_id);
-  $stmt4->close();
-
- $array = array();
-  $stmt = $mysqli->prepare("SELECT news_id, story, local, path, user_id,last_update FROM news order by last_update DESC");
-  if(!$stmt){
-  printf("Query Prep Failed: %s\n", $mysqli->error);
-  exit;
-  }
-  $stmt->execute();
-  $stmt->bind_result($news_id, $text, $local, $path, $user_id, $date_story);
-  while($stmt->fetch()){
-    array_push($array, htmlspecialchars($news_id), htmlspecialchars($user_id), htmlspecialchars($text), htmlspecialchars($path), htmlspecialchars($date_story));
-}
-$stmt->close();
-echo "<ul>\n";
-    $arr_length = count($array);
-    for($i=0;$i<$arr_length;$i+=5)
-{
-    $news_id2 = $array[$i];	  
-    $user_id2 = $array[$i+1];	  
-    $text2 = $array[$i+2];	  
-    $path2 = $array[$i+3];	   
-    $date_story2 = $array[$i+4];	   
-    $stmt2 = $mysqli->prepare("SELECT username FROM registered_users WHERE id=?");
-    $stmt2->bind_param('i', $user_id2);
-    $stmt2->execute();
-    $stmt2->bind_result($author);
-    $stmt2->fetch();
-    $stmt2->close();
-    if ($path2 != ""){
-    printf("<div class='holder'><div class='story'>%s</div>\n<a href=%s> %s </a>\n<div class='author'>Published by %s on %s</div>\n<div class='label'>Comments:</div><ul>\n",$text2,$path2,$path2,$author, $date_story2);
-    }
-    else{
-    printf("<div class='holder'><div class='story'>%s</div>\n<div class='author'>Published by %s on %s</div>\n<div class='label'>Comments:</div><ul>\n",$text2,$author, $date_story2);
-    }
-    $comments_array = array();
-    $stmt3 = $mysqli->prepare("SELECT comments_id, comment, user_id FROM comments WHERE news_id=?");	      
-    $stmt3->bind_param('i', $news_id2);
-    $stmt3->execute();
-    $stmt3->bind_result($comments_id, $comment, $commenter_id); 
-    while($stmt3->fetch()){
-	  array_push($comments_array, htmlspecialchars($comment), htmlspecialchars($commenter_id));
-      }
-      $stmt3->close();		    
-      $arr_length2 = count($comments_array);
-    for($j=0;$j<$arr_length2;$j+=2)
-    {
-    $stmt4 = $mysqli->prepare("SELECT username FROM registered_users WHERE id=?");
-    $stmt4->bind_param('i', $comments_array[$j+1]);
-    $stmt4->execute();
-    $stmt4->bind_result($commenter);
-    $stmt4->fetch();
-    $stmt4->close();
-    printf("<div class='comment'><div class='story'>%s</div><div class='author'>Comment by %s</div></div>\n", $comments_array[$j],$commenter);
-    }
-    echo "</ul></div>\n";
-}	    
-  echo "</ul>\n";  
-
-   ?>
 </div>
-
+<script>
+ $("#login").click( function(){
+ $(".userLoginDetails").show();
+});
+  $("#createUser").click( function(){
+ $(".userCreateDetails").show();
+});
+ $("#createUser").click( function(){
+ $(".userCreateDetails").show();
+});
+  $("#shareCalendar").click( function(){
+ $(".shareDetails").show();
+});
+  $("#createEvent").click( function(){
+ $(".eventCreateDetails").show();
+});
+$("#submitCreateUser").click( function(){
+ var newusrname = $("#newUsername").val();
+ var newusrpass = $("#newPassword").val();
+ var pdata = {
+ newUsername : newusrname,
+ newPassword : newusrpass
+ };
+ $.ajax({type:'POST', url: 'createuser_ajax.php', data: pdata}).done( function(response) {
+ alert("success");
+ });
+});
+</script>
 </body>
 </html>
 
