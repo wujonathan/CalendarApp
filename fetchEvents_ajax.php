ee0766063714650340057aa9e4341e335dd5e72b
Jonathan Wu <wu.jonathan@wustl.edu>
2016-03-26 22:10:50 -0500

    Fetch events

--- fetchEvents_ajax.php
+++ fetchEvents_ajax.php
@@ -4,6 +4,7 @@ header("Content-Type: application/json");
 session_start();
 require 'database.php';
 $user_id=$_SESSION['user_id'];
+$queryMonth=$_POST["queryMonth"];
 
 $shared_id;
 $users_array=array($user_id);
@@ -23,18 +24,18 @@ while($stmt->fetch()){
 $stmt->close();
 $events_array=array();
 for($i=0;$i<sizeof($users_array);$i++){
-	$stmt = $mysqli->prepare("SELECT title, description, time, FROM events WHERE user_id=?");
+	$stmt = $mysqli->prepare("SELECT title, description, time, FROM events WHERE user_id=? AND month=?");
 	if(!$stmt){
 		echo json_encode(array(
 			"success" => false,
 			"message" => $mysqli->error						));
 		exit;
 	}
-	$stmt->bind_param('s', $users_array[$i]);
+	$stmt->bind_param('ss', $users_array[$i], $queryMonth);
 	$stmt->execute();
-	$stmt->bind_result($curTitle, $curDescription, $curTime);
+	$stmt->bind_result($curTitle, $curDescription, $curDay, $curTime);
 	while($stmt->fetch()){
-		array_push($events_array,  htmlspecialchars($curTitle), htmlspecialchars($curDescription), htmlspecialchars($curTime));
+		array_push($events_array, htmlspecialchars($curTitle), htmlspecialchars($curDescription), htmlspecialchars($curTime), htmlspecialchars($curDay));
 	}
 	$stmt->close();
 }
